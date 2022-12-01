29056660fbc2b4a55d4fbebef67dc5019e275145
ssicari <74316868+ssicari@users.noreply.github.com>
2022-11-30 22:59:02 -0500

    Update lab 3.ino

--- lab 3.ino
+++ lab 3.ino
@@ -1,4 +1,12 @@
 //code modified from Elegoo website
+#include <Wire.h>
+#include <DS3231.h>
+#include <LiquidCrystal.h>
+
+DS3231 clock;
+RTCDateTime dt;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
 #define ENABLE 5
 #define DIR1 3
 #define DIR2 4
@@ -9,16 +17,32 @@ void setup()
   pinMode(ENABLE, OUTPUT);
   pinMode(DIR1, OUTPUT);
   pinMode(DIR2, OUTPUT);
-  Serial.begin(9600);
+  lcd.begin(9600);
+
+  clock.begin();
+  clock.setDateTime(__DATE__, __TIME__);
+
+  lcd.begin(16, 2);
 }
 
 void loop() 
 {
+  lcd.setCursor(0, 1);
+  dt = clock.getDateTime();
+
   while(1) //infinite loop to keep fan running
   {
     for(int i = 0; i < 5; i++) //iterate through all 5 speeds (full, 3/4, 1/2. 1/4, off)
     {
       //digitalWrite(ENABLE, HIGH);
+
+      lcd.print(dt.year);   lcd.print("-");
+      lcd.print(dt.month);  lcd.print("-");
+      lcd.print(dt.day);    lcd.print(" ");
+      lcd.print(dt.hour);   lcd.print(":");
+      lcd.print(dt.minute); lcd.print(":");
+      lcd.print(dt.second); lcd.println("");
+
       analogWrite(ENABLE, fan_speeds[i]);
       digitalWrite(DIR1, HIGH); //start fan spinning in one direction
       digitalWrite(DIR2, LOW);
@@ -27,7 +51,7 @@ void loop()
           digitalWrite(DIR1, LOW); //change fan direction
           digitalWrite(DIR2, HIGH);
         }
-      delay(2000);
+      delay(1000);
     }
 
   }
