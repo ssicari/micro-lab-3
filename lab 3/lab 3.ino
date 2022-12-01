0ddef65f4adc8534fbb11b3d2c60637d901e30d5
ryan-donald <bluefalcon0990@gmail.com>
2022-12-01 18:53:39 -0500

    updates/file fix

--- lab 3/lab 3.ino
+++ lab 3/lab 3.ino
@@ -12,17 +12,30 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 #define DIR2 4
 uint8_t fan_speeds[] = {255, 180, 128, 50, 0};
 
+bool motor_dir1 = HIGH
+bool motor_dir2 = LOW
+
+attachInterrupt(digitalPinToInterrupt(2), buttonISR, RISING)
+
 void setup() 
 {
   pinMode(ENABLE, OUTPUT);
   pinMode(DIR1, OUTPUT);
   pinMode(DIR2, OUTPUT);
-  lcd.begin(9600);
+  serial.begin(9600);
+  //lcd.begin(9600);
 
   clock.begin();
   clock.setDateTime(__DATE__, __TIME__);
 
   lcd.begin(16, 2);
+}
+
+void motorcontrol(byte speed, bool D1, bool D2)
+{
+  analogWrite(ENABLE, speed);
+  digitalWrite(DIR1, D1); //start fan spinning in one direction
+  digitalWrite(DIR2, D2);
 }
 
 void loop() 
@@ -43,17 +56,24 @@ void loop()
       lcd.print(dt.minute); lcd.print(":");
       lcd.print(dt.second); lcd.println("");
 
-      analogWrite(ENABLE, fan_speeds[i]);
-      digitalWrite(DIR1, HIGH); //start fan spinning in one direction
-      digitalWrite(DIR2, LOW);
-      if(//button is pressed)
+      motor_control(fan_speeds[i], motor_dir1, motor_dir2)
+      //analogWrite(ENABLE, fan_speeds[i]);
+      //digitalWrite(DIR1, HIGH); //start fan spinning in one direction
+      //digitalWrite(DIR2, LOW);
+      
+      /*if(//button is pressed)
         {
           digitalWrite(DIR1, LOW); //change fan direction
           digitalWrite(DIR2, HIGH);
         }
-      delay(1000);
+      delay(1000);*/
     }
 
   }
+
+void buttonISR() {
+  motor_dir1 = !motor_dir1
+  motor_dir2 = !motor_dir2
+}
 
 }
